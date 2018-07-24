d596f3c4f877cb2446716aadd443667df543bb39
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-24 18:29:27 -0400

    Modified comments regrading account creation, login and logout. And and exported router using module.exports.

--- routes/user.js
+++ routes/user.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.js');
 
+// Come back and complete account login, signUp, logout and show later...
+
+
+
+
+module.exports = router
