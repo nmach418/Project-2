62e632bd1bded703b0d1e2e74f4037c1a0ccc969
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-24 19:08:01 -0400

    Updated index.js to have serving listening on port 3500. Added router to the app instance of Express with the .use() method. Also, used .use and .set to add other items.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const methodOverride = require("method-override");
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true}));
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(require("./routes/index.js"));
+app.use(methodOverride("_method"));
+
+app.listen(3500, () => console.log("this server is running like greased lightning"));
