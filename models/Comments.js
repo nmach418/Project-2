1654dc1184a546a1440daa9615a22b41ff470791
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-25 19:56:53 -0400

    Built data model in models/Comments.js file.

---
+++ models/Comments.js
@@ -0,0 +1,22 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const Comments = new Schema({
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+
+  // comments: [Comment]     // Discuss for reinforcement. Code no longer applies. Comments were 'in the same model as Tweets ' in the 'two model' Tweeter app. Now we've built a third model. 
+
+
+module.exports = {
+  Comments: mongoose.model("Comments", Comments)
+};
