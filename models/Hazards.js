24091eb7fa184f020084680d67b1a836a5464d96
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-25 19:35:06 -0400

    Built data model in models/Hazards.js file.

---
+++ models/Hazards.js
@@ -0,0 +1,21 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const Hazards = new Schema({
+  waterwayName: String,
+  location: String,
+  latLng: Integer,      //if available
+  hazDesc: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+module.exports = {
+  Hazards: mongoose.model("Hazards", Hazards)
+};
