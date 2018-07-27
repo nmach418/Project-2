45e482f3175138cb1897e4b67c9b0309812b7c53
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-27 17:45:13 -0400

    Removed authentication requirement from route paths.

--- routes/hazards.js
+++ routes/hazards.js
@@ -4,9 +4,25 @@ const express = require("express");
 const router = express.Router();
 const hazardsController = require("../controllers/hazards");
 
-router.post("/", hazardsController.requireAuth, hazardsController.create);
-router.get("/new", hazardsController.requireAuth, hazardsController.new);
+router.post("/", hazardsController.create);
+router.get("/new", hazardsController.new);
 router.get("/:id", hazardsController.show);
-router.put("/:id", hazardsController.requireAuth, hazardsController.update);
+router.put("/", hazardsController.update);
+router.put("/:id", hazardsController.update);
+
+module.exports = router;
+
+
+// !!!! Refer to Twitter View not the final solution!!! REMOVE requireAuth
+
+const express = require("express");
+const router = express.Router();
+const tweetController = require("../controllers/tweet");
+
+router.post("/", tweetController.create);
+router.get("/new", tweetController.new);
+router.get("/:id", tweetController.show);
+router.put("/", tweetController.update);
+router.put("/:id", tweetController.update);
 
 module.exports = router;
