85754904db82a47ef6a9c6f098a21500423eeabe
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-27 15:46:50 -0400

    Created routes for create, new, show, and update operations (covering 3 of the 4 CRUD operations).

--- routes/hazards.js
+++ routes/hazards.js
@@ -1,1 +1,12 @@
 // Think CRUD
+
+const express = require("express");
+const router = express.Router();
+const hazardsController = require("../controllers/hazards");
+
+router.post("/", hazardsController.requireAuth, hazardsController.create);
+router.get("/new", hazardsController.requireAuth, hazardsController.new);
+router.get("/:id", hazardsController.show);
+router.put("/:id", hazardsController.requireAuth, hazardsController.update);
+
+module.exports = router;
