531bf9db19e9a04985eb4a9f84fffca23cb24e8f
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-30 13:31:17 -0400

    Just adjusted line spacing in index.js file.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const hbs = require("hbs");
 const methodOverride = require("method-override");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
-hbs.registerPartials(__dirname + "/views/partials");
-app.set("view engine", "hbs");
-app.use(methodOverride("_method"));
-app.use(require("./routes/index.js"));
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public")); // Research
+hbs.registerPartials(__dirname + "/views/partials"); // Research
+app.set("view engine", "hbs");
+app.use(methodOverride("_method"));  // Review
+app.use(require("./routes/index.js"));
 
 app.listen(3500, () => console.log("this server is running like greased lightning"));
