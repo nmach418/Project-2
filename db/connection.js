cc0bce46b0ba903982ea759ebef174269e0b3d48
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-30 14:41:00 -0400

    Updated comment in db/connection file.

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 // Connection to navhazards db
 const  mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/navhaz");  // Discuss navhazards db name
-mongoose.Promise = Promise;   // Research Promise again
+mongoose.Promise = Promise;   // Research Promise again - used in place of callbacks
 
 module.exports = mongoose;
