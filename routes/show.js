ea3e72720b253f6c6492ea5a23d20d43d53bb857
Boku <Boku@NHs-MacBook-Pro.local>
2018-08-02 16:03:52 -0400

    Removed temp test routes/show.js file.

--- routes/show.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const showController = require('../controllers/show.js')
-
-router.get('/', showController.showgo);
-
-module.exports = router;  // delete show.js file
