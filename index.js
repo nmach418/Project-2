d10ae04e5fbc9ffad8a56bbd0a39030d782e81ef
Boku <Boku@NHs-MacBook-Pro.local>
2018-08-03 09:00:42 -0400

    Removed comments

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const methodOverride = require("method-override");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static("public")); // Research
-hbs.registerPartials(__dirname + "/views/partials"); // Research
+app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
-app.use(methodOverride("_method"));  // Review
+app.use(methodOverride("_method"));
 app.use(require("./routes/index.js"));
 
 app.listen(3500, () => console.log("this server is plugging along"));
