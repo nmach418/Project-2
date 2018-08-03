0af34e564f861bed8bbea13825f9f44aad92feca
Boku <Boku@NHs-MacBook-Pro.local>
2018-08-03 09:03:37 -0400

    Removed commented-out code.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./application.js')); // ask about .js on this line only
+router.use('/', require('./application.js'));
 router.use('/user', require('./user'));
 router.use('/hazard', require('./hazard'));
-
-// router.use('/', require('./hazard'));
-
 
 router.all('*', (req, res) => {             
     res.status(400).send();
